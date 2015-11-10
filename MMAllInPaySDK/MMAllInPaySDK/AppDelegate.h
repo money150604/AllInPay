870379882cfe9bc039ea6f7ffae196d57b1b9e3b
money150604 <80146944@qq.com>
2015-11-10 11:20:00 +0800

    增加SDK包

---
+++ MMAllInPaySDK/MMAllInPaySDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMAllInPaySDK
+//
+//  Created by LongXiTechhnology on 15/11/10.
+//  Copyright © 2015年 LongXiTechhnology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
